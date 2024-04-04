77ea6465c95383d0f82bf2aa0856d87330665b32
bsjhx <rakoczymichal@hotmail.com>
2024-04-04 22:30:24 +0200

    2. Read Data From The Solana Network

---
+++ generate-keypair/check-balance.ts
@@ -0,0 +1,23 @@
+import { Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from '@solana/web3.js';
+
+let publicKey: PublicKey | undefined;
+try {
+    publicKey = new PublicKey('GgJJRwLg9NzFQ97o1CJLGLp1KLSUMBwFc6eQNVEr4fbW');
+} catch (error) {
+    console.log('Wrong address!');
+}
+
+if (publicKey) {
+    const connection = new Connection('https://api.devnet.solana.com', 'confirmed');
+    const connectionWithClusterUrl = new Connection(clusterApiUrl('mainnet-beta'));
+
+    const balanceInLamports = await connectionWithClusterUrl.getBalance(publicKey);
+
+    const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+    console.log(
+        `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
+    );
+}
+
+// GgJJRwLg9NzFQ97o1CJLGLp1KLSUMBwFc6eQNVEr4fbW
