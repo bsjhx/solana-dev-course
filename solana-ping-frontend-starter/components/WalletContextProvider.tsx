de7bcbbae0577e8e08162c57bf6e12347be3bcc7
bsjhx <rakoczymichal@hotmail.com>
2024-04-08 21:50:10 +0200

    5. Interact With Wallets

---
+++ solana-ping-frontend-starter/components/WalletContextProvider.tsx
@@ -0,0 +1,34 @@
+import { FC, ReactNode, useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import * as web3 from "@solana/web3.js";
+import * as walletAdapterWallets from "@solana/wallet-adapter-wallets";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => web3.clusterApiUrl(network), [network]);
+  
+  const wallets = useMemo(
+    () => [
+        new PhantomWalletAdapter(),
+        new SolflareWalletAdapter()
+    ],
+    []
+  );  
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
