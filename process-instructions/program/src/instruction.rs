9c3a2e54ae8c7556a70fdc2381d418e57553aa23
bsjhx <rakoczymichal@hotmail.com>
2024-04-19 19:49:36 +0200

    State management

--- process-instructions/program/src/instruction.rs
+++ process-instructions/program/src/instruction.rs
@@ -9,22 +9,26 @@ pub enum MovieInstruction {
     },
 }
 
+#[derive(BorshDeserialize)]
+struct MovieReviewPayload {
+    title: String,
+    rating: u8,
+    description: String,
+}
+
 impl MovieInstruction {
-    // Unpack inbound buffer to associated Instruction
-    // The expected format for input is a Borsh serialized vector
     pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
-        // Split the first byte of data
-        let (&variant, rest) = input.split_first().ok_or(ProgramError::InvalidInstructionData)?;
-        // `try_from_slice` is one of the implementations from the BorshDeserialization trait
-        // Deserializes instruction byte data into the payload struct
+        let (&variant, rest) = input
+            .split_first()
+            .ok_or(ProgramError::InvalidInstructionData)?;
         let payload = MovieReviewPayload::try_from_slice(rest).unwrap();
-        // Match the first byte and return the AddMovieReview struct
         Ok(match variant {
             0 => Self::AddMovieReview {
                 title: payload.title,
                 rating: payload.rating,
-                description: payload.description },
-            _ => return Err(ProgramError::InvalidInstructionData)
+                description: payload.description,
+            },
+            _ => return Err(ProgramError::InvalidInstructionData),
         })
     }
 }
--- process-instructions/program/src/lib.rs
+++ process-instructions/program/src/lib.rs
@@ -1,13 +1,20 @@
 use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    borsh::try_from_slice_unchecked,
     entrypoint,
     entrypoint::ProgramResult,
-    pubkey::Pubkey,
     msg,
-    account_info::AccountInfo,
+    program::invoke_signed,
+    pubkey::Pubkey,
+    system_instruction,
+    sysvar::{rent::Rent, Sysvar},
 };
-
+use std::convert::TryInto;
 pub mod instruction;
-use instruction::{MovieInstruction};
+pub mod state;
+use borsh::BorshSerialize;
+use instruction::MovieInstruction;
+use state::MovieAccountState;
 
 // Entry point is a function call process_instruction
 entrypoint!(process_instruction);
@@ -16,13 +23,17 @@ entrypoint!(process_instruction);
 pub fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
-    instruction_data: &[u8]
+    instruction_data: &[u8],
 ) -> ProgramResult {
     // Unpack called
     let instruction = MovieInstruction::unpack(instruction_data)?;
     // Match against the data struct returned into `instruction` variable
     match instruction {
-        MovieInstruction::AddMovieReview { title, rating, description } => {
+        MovieInstruction::AddMovieReview {
+            title,
+            rating,
+            description,
+        } => {
             // Make a call to `add_move_review` function
             add_movie_review(program_id, accounts, title, rating, description)
         }
@@ -34,14 +45,63 @@ pub fn add_movie_review(
     accounts: &[AccountInfo],
     title: String,
     rating: u8,
-    description: String
+    description: String,
 ) -> ProgramResult {
-
     // Logging instruction data that was passed in
     msg!("Adding movie review...");
     msg!("Title: {}", title);
     msg!("Rating: {}", rating);
     msg!("Description: {}", description);
+
+    let account_info_iter = &mut accounts.iter();
+    let initializer = next_account_info(account_info_iter)?;
+    let pda_account = next_account_info(account_info_iter)?;
+    let system_program = next_account_info(account_info_iter)?;
+
+    // Derive PDA
+    let (pda, bump_seed) = Pubkey::find_program_address(
+        &[initializer.key.as_ref(), title.as_bytes().as_ref()],
+        program_id,
+    );
+    let account_len: usize = 1 + 1 + (4 + title.len()) + (4 + description.len());
+    let rent = Rent::get()?;
+    let rent_lamports = rent.minimum_balance(account_len);
+
+    invoke_signed(
+        &system_instruction::create_account(
+            initializer.key,
+            pda_account.key,
+            rent_lamports,
+            account_len.try_into().unwrap(),
+            program_id,
+        ),
+        &[
+            initializer.clone(),
+            pda_account.clone(),
+            system_program.clone(),
+        ],
+        &[&[
+            initializer.key.as_ref(),
+            title.as_bytes().as_ref(),
+            &[bump_seed],
+        ]],
+    )?;
+
+    msg!("PDA created: {}", pda);
+
+    msg!("unpacking state account");
+    let mut account_data =
+        try_from_slice_unchecked::<MovieAccountState>(&pda_account.data.borrow()).unwrap();
+    msg!("borrowed account data");
+
+    account_data.title = title;
+    account_data.rating = rating;
+    account_data.description = description;
+    account_data.is_initialized = true;
+
+    msg!("serializing account");
+    account_data.serialize(&mut &mut pda_account.data.borrow_mut()[..])?;
+    msg!("state account serialized");
 
     Ok(())
 }
