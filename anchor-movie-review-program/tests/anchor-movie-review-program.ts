f35d1172438cfccb9f6fbef7494bcad18b6e871b
bsjhx <rakoczymichal@hotmail.com>
2024-06-08 23:10:49 +0200

    Anchor CPIs and Errors

--- anchor-movie-review-program/tests/anchor-movie-review-program.ts
+++ anchor-movie-review-program/tests/anchor-movie-review-program.ts
@@ -1,9 +1,11 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { assert, expect } from "chai"
+import * as anchor from "@coral-xyz/anchor"
+import { Program } from "@coral-xyz/anchor"
+import { expect } from "chai"
+import { getAssociatedTokenAddress, getAccount } from "@solana/spl-token"
 import { AnchorMovieReviewProgram } from "../target/types/anchor_movie_review_program"
 
 describe("anchor-movie-review-program", () => {
+
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env()
   anchor.setProvider(provider)
@@ -16,22 +18,42 @@ describe("anchor-movie-review-program", () => {
     description: "Wow what a good movie it was real great",
     rating: 5,
   }
-  const [moviePda] = anchor.web3.PublicKey.findProgramAddressSync(
+
+  const [movie_pda] = anchor.web3.PublicKey.findProgramAddressSync(
     [Buffer.from(movie.title), provider.wallet.publicKey.toBuffer()],
     program.programId
   )
 
+  const [mint] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("mint")],
+    program.programId
+  )
+
+  it("Initializes the reward token", async () => {
+    const tx = await program.methods.initializeTokenMint().rpc()
+  })
+
   it("Movie review is added`", async () => {
-    // Add your test here.
+    const tokenAccount = await getAssociatedTokenAddress(
+      mint,
+      provider.wallet.publicKey
+    )
+
     const tx = await program.methods
       .addMovieReview(movie.title, movie.description, movie.rating)
+      .accounts({
+        tokenAccount: tokenAccount,
+      })
       .rpc()
 
-    const account = await program.account.movieAccountState.fetch(moviePda)
+    const account = await program.account.movieAccountState.fetch(movie_pda)
     expect(movie.title === account.title)
     expect(movie.rating === account.rating)
     expect(movie.description === account.description)
     expect(account.reviewer === provider.wallet.publicKey)
+
+    const userAta = await getAccount(provider.connection, tokenAccount)
+    expect(Number(userAta.amount)).to.equal((10 * 10) ^ 6)
   })
 
   it("Movie review is updated`", async () => {
@@ -42,7 +64,7 @@ describe("anchor-movie-review-program", () => {
       .updateMovieReview(movie.title, newDescription, newRating)
       .rpc()
 
-    const account = await program.account.movieAccountState.fetch(moviePda)
+    const account = await program.account.movieAccountState.fetch(movie_pda)
     expect(movie.title === account.title)
     expect(newRating === account.rating)
     expect(newDescription === account.description)
